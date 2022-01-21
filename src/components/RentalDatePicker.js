4d15b857872807c4855f944db0ce456d09427610
Andrew Jyan <andrew.jyan@ibm.com>
2022-01-21 16:07:57 -0500

    added drop down and formatting

--- src/components/RentalDatePicker.js
+++ src/components/RentalDatePicker.js
@@ -2,18 +2,35 @@ import React from "react";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import { DatePicker } from "@blueprintjs/datetime";
+import { Button } from "@blueprintjs/core";
 
-function RentalDatePicker({ type }) {
+import { Classes, Popover2 } from "@blueprintjs/popover2";
+
+function RentalDatePicker({ type, setPickupDate, setDropoffDate }) {
   return (
-    <div
-      style={{
-        display: "block",
-        width: 400,
-        padding: 30,
-      }}
-    >
-      <h4>Select a {type} Date</h4>
-      <DatePicker />
+    <div>
+      <Popover2
+        interactionKind="click"
+        popoverClassName={Classes.POPOVER2_CONTENT_SIZING}
+        placement="bottom"
+        content={
+          <DatePicker
+            onChange={(selectedDate) => {
+              type === "pickup]"
+                ? setPickupDate(selectedDate)
+                : setDropoffDate(selectedDate);
+            }}
+          />
+        }
+        renderTarget={({ isOpen, ref, ...targetProps }) => (
+          <Button
+            {...targetProps}
+            elementRef={ref}
+            intent="primary"
+            text={`Select a ${type} date`}
+          />
+        )}
+      />
     </div>
   );
 }
--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../styles/DigitalMesh.css";
-import { Card, InputGroup, Button, MenuItem } from "@blueprintjs/core";
+import { Card, Button } from "@blueprintjs/core";
 import { TimePicker } from "@blueprintjs/datetime";
 import RentalDatePicker from "./RentalDatePicker";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
@@ -13,45 +13,52 @@ const locations = [
 ];
 
 const SubmitForm = () => {
-  const [selectedLocation, setSelectedLocation] = useState(locations[0]);
-  const handleClick = () => {
-    console.log("clicked");
-  };
+  const [pickupLocation, setPickupLocation] = useState(locations[0]);
+  const [pickupDate, setPickupDate] = useState(); // format date
+  const [pickupTime, setPickupTime] = useState();
+  const [dropoffLocation, setDropoffLocation] = useState();
+  const [dropoffDate, setDropoffDate] = useState(); // format date
+  const [dropoffTime, setDropoffTime] = useState();
 
   return (
     <div>
       <div className="card-container">
         <Card className="car-card">
-          <h5>
-            <a href="#">IBM_1</a>
-          </h5>
-          <p>Card content</p>
-          <div className="drop-down-menu">
-            <div class="bp3-html-select">
-              <select>
-                <option selected>Choose a pick up location</option>
-                {locations.map((location) => {
-                  return <option>{location.name}</option>;
-                })}
-              </select>
-              <span class="bp3-icon bp3-icon-double-caret-vertical"></span>
-            </div>
-            {/* {add a pop up menu for datepicker} */}
-            <RentalDatePicker type={"Pickup"} />
-
-            <TimePicker />
-            <div class="bp3-html-select">
-              <select>
-                <option selected>Choose a drop off location</option>
-                {locations.map((location) => {
-                  return <option>{location.name}</option>;
-                })}
-              </select>
-              <span class="bp3-icon bp3-icon-double-caret-vertical"></span>
-            </div>
-            <RentalDatePicker type={"Drop off"} />
-            <TimePicker />
+          <div class="bp3-html-select">
+            <select>
+              <option selected>Choose a pick up location</option>
+              {locations.map((location) => {
+                return <option>{location.name}</option>;
+              })}
+            </select>
+            <span class="bp3-icon bp3-icon-double-caret-vertical"></span>
           </div>
+
+          <RentalDatePicker
+            type={"Pickup"}
+            setPickupDate={setPickupDate}
+            setDropoffDate={setDropoffDate}
+          />
+
+          <TimePicker useAmPm={true} />
+          <div class="bp3-html-select">
+            <select>
+              <option selected>Choose a drop off location</option>
+              {locations.map((location) => {
+                return <option>{location.name}</option>;
+              })}
+            </select>
+            <span class="bp3-icon bp3-icon-double-caret-vertical"></span>
+          </div>
+
+          <RentalDatePicker
+            type={"Drop off"}
+            setPickupDate={setPickupDate}
+            setDropoffDate={setDropoffDate}
+          />
+
+          <TimePicker useAmPm={true} />
+
           <Button>Submit</Button>
         </Card>
       </div>
