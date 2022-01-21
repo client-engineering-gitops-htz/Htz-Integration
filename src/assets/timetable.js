d5818a764842a78efb5f2887272ee6cce228e216
laurenmcn <lauren.mcnabb@ibm.com>
2022-01-21 14:03:44 -0500

    added styling

---
+++ src/assets/timetable.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// This is for the Select options to map
+const timetable = [
+    { time: "12:30 AM"},
+    { time: "1:00 AM"},
+    { time: "12:30 AM"},
+    { time: "12:30 AM"},
+
+];
+
+
+export default timetable
--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -13,26 +13,17 @@ const SubmitForm=() => {
                     <a href="#">IBM_1</a>
                 </h5>
                 <p>Card content</p>
-                <Button>Submit</Button>
-                </Card>
-            </div>
-            <Card className='submit-card'>
-                {/* Gonna have to switch this to blueprint Select: 
-                https://blueprintjs.com/docs/#select/select-component.querying */}
-                <div>
-                    For this section: we want to have Select for PU/DO Locations and 
-                    then Date Picker and Time Picker for PU/DO date and time as seen 
-                    in: https://codesandbox.io/s/blueprint-sandbox-et9xy?file=/src/SelectExample.tsx
-                </div>
+                <div className='drop-down-menu'>
                 <InputGroup type="search" placeholder='Pick Up Location' />
                 <InputGroup type="search" placeholder='Pick Up Date' />
                 <InputGroup type="search" placeholder='Pick Up Time' />
                 <InputGroup type="search" placeholder='Drop Off Location' />
                 <InputGroup type="search" placeholder='Drop Off Date' />
                 <InputGroup type="search" placeholder='Drop Off Time' />
-
-            </Card>
-            
+                </div>
+                <Button>Submit</Button>
+                </Card>
+            </div>
 
         </div>
 
