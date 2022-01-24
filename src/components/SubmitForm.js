4b43ab7defb50deac0f0c3759f3067c1b382678b
Andrew Jyan <andrew.jyan@ibm.com>
2022-01-24 16:51:10 -0500

    added state for pickup and dropoff dates

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "../styles/DigitalMesh.css";
-import { Card, Button } from "@blueprintjs/core";
+import { Card, Button, HTMLSelect } from "@blueprintjs/core";
 import { TimePicker } from "@blueprintjs/datetime";
 import RentalDatePicker from "./RentalDatePicker";
+import LocationPicker from "./LocationPicker";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import { getDateOnlyWithTime } from "@blueprintjs/datetime/lib/esm/common/dateUtils";
@@ -14,7 +15,7 @@ const locations = [
 ];
 
 const SubmitForm = () => {
-  const [pickupLocation, setPickupLocation] = useState(locations[0]);
+  const [pickupLocation, setPickupLocation] = useState();
   const [pickupDate, setPickupDate] = useState(); // format date
   const [pickupTime, setPickupTime] = useState();
   const [dropoffLocation, setDropoffLocation] = useState();
@@ -24,53 +25,55 @@ const SubmitForm = () => {
   return (
     <div>
       <div className="card-container">
-        <div className="trip-header">
-            Book Your Trip
-        </div>
+        <div className="trip-header">Book Your Trip</div>
         <Card className="car-card">
-        <div className="trip-details">
-          <div class="bp3-html-select">
-            <select className="select-location">
-              <option selected>Choose pick up location</option>
-              {locations.map((location) => {
-                return <option>{location.name}</option>;
-              })}
-            </select>
-            {/* Fix center alignment on caret */}
-            <span class="bp3-icon bp3-icon-double-caret-vertical"></span>   
-          </div>
-          <div class="bp3-html-select">
-            <select className="select-location">
-              <option selected>Choose drop off location</option>
-              {locations.map((location) => {
-                return <option>{location.name}</option>;
-              })}
-            </select>
-            {/* Fix center alignment on caret */}
-            <span class="bp3-icon bp3-icon-double-caret-vertical"></span>
-          </div>
+          <div className="trip-details">
+            <LocationPicker
+              type={"Pick-Up"}
+              setPickupLocation={setPickupLocation}
+              setDropoffLocation={setDropoffLocation}
+            />
 
-          <div className="date-time-details">
-          <RentalDatePicker
-            type={"Pickup"}
-            setPickupDate={setPickupDate}
-            setDropoffDate={setDropoffDate}
-          />
-          {/* Fix center alignment on digits */}
-          <TimePicker />
-          </div>
-          
-          <div className="date-time-details">
-          <RentalDatePicker
-            type={"Drop off"}
-            setPickupDate={setPickupDate}
-            setDropoffDate={setDropoffDate}
-          />
-          {/* Fix center alignment on caret */}
-          <TimePicker/>
-          </div>
+            <LocationPicker
+              type={"Drop-Off"}
+              setPickupLocation={setPickupLocation}
+              setDropoffLocation={setDropoffLocation}
+            />
 
-          <Button className="submit-button">Submit</Button>
+            <div className="date-time-details">
+              <RentalDatePicker
+                type={"Pick-Up"}
+                setPickupDate={setPickupDate}
+                setDropoffDate={setDropoffDate}
+                pickupDate={pickupDate}
+              />
+              {/* Fix center alignment on digits */}
+              <TimePicker />
+            </div>
+
+            <div className="date-time-details">
+              <RentalDatePicker
+                type={"Drop-Off"}
+                setPickupDate={setPickupDate}
+                setDropoffDate={setDropoffDate}
+                dropoffDate={dropoffDate}
+              />
+              {/* Fix center alignment on caret */}
+              <TimePicker />
+            </div>
+
+            <Button
+              className="submit-button"
+              onClick={() => {
+                console.log("pickup", pickupLocation);
+                console.log("dropoff", dropoffLocation);
+
+                console.log("pickup date", pickupDate);
+                console.log("dropoff date", dropoffDate);
+              }}
+            >
+              Submit
+            </Button>
           </div>
         </Card>
       </div>
