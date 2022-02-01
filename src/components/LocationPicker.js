24eb2734eef71670ab5e80772134855679ad136c
Matt Womack <matthew.womack@ibm.com>
2022-01-31 19:26:28 -0600

    added filter UI to pickup and dropoff inputs

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { HTMLSelect } from "@blueprintjs/core";
-const usLocations = require('../assets/ATPxref_US_first_20.json');
+import { Select } from "@blueprintjs/select";
+import SelectLocation from "./SelectLocation";
+/*
 const locations = [
   { name: "JFK", id: 1 },
   { name: "LGA", id: 2 },
   { name: "DFW", id: 3 },
 ];
-
+*/
 function LocationPicker({ type, setPickupLocation, setDropoffLocation }) {
   return (
-    <HTMLSelect
-      onChange={(e) => {
+    <SelectLocation
+      handleValueChange ={(e) => {
+        console.log(`here's the event: ${e}`);
         if (type === "Pick-Up") setPickupLocation(e.target.value);
         if (type === "Drop-Off") setDropoffLocation(e.target.value);
       }}
     >
-      <option selected>{`Choose ${type} location`}</option>
-      {usLocations.map((usLocations) => {
-        return <option value={usLocations.OAG_CODE}>{usLocations.LOC_NM}</option>;
-      })}
-    </HTMLSelect>
+    <option selected>{`Choose ${type} location`}</option>
+    </SelectLocation>
   );
 }
-
+/*
+   {usLocations.map((usLocations) => {
+        return <option value={usLocations.OAG_CODE}>{usLocations.LOC_NM}</option>;
+      })}
+*/
 export default LocationPicker;
---
+++ src/components/SelectLocation.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Button, MenuItem, H5 } from "@blueprintjs/core";
+import { Select } from "@blueprintjs/select";
+import * as Locations from "./locations";
+
+const LocationSelect = Select.ofType();
+
+class SelectLocation extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: Locations.importedUSLocations[0]
+    };
+  }
+  render() {
+    const buttonText = this.state.location.LOC_NM || `Selection Location`;
+    return (
+      <div>
+        {this.props.label && <H5>{this.props.label}</H5>}
+        <LocationSelect
+          fill
+          items={Locations.importedUSLocations}
+          itemPredicate={Locations.filterLocation}
+          itemRenderer={Locations.renderLocation}
+          noResults={<MenuItem disabled={true} text="No results." />}
+          onItemSelect={this.handleValueChange}
+        >
+          <Button
+            intent={this.props.intent}
+            text={buttonText}
+            rightIcon="caret-down"
+          />
+        </LocationSelect>
+      </div>
+    );
+  }
+
+  handleValueChange = location => {
+    console.log(location);
+    this.setState({ location: location });
+    if (this.props.setLocation) {
+      this.props.setLocation(location.OAG_CODE);
+    }
+  };
+}
+
+export default SelectLocation;
